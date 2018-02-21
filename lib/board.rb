68a63d62560a3df551bd440261df1b0ae0d94c55
Kiwi Chi <kiwi.chi@wework.com>
2018-02-21 09:08:23 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -2,15 +2,4 @@
 
 board = [" "," "," "," "," "," "," "," "," "]
 
-def display_board
-
-puts " 1 | 2 | 3 "
-puts "------------"
-puts " 4 | 5 | 6 "
-puts "------------"
-puts " 7 | 8 | 9  "
-end
-
-
-puts display_board
 
