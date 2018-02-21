22130745afc50dc7c80c9f894b2394ef34f70003
Kiwi Chi <kiwi.chi@wework.com>
2018-02-21 09:08:16 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,16 @@
 # Define the variable board below.
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board
+
+puts " 1 | 2 | 3 "
+puts "------------"
+puts " 4 | 5 | 6 "
+puts "------------"
+puts " 7 | 8 | 9  "
+end
+
+
+puts display_board
+
